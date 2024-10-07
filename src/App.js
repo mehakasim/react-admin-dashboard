b0af9fa0b378145592ca2c6939319c6b92ddd40d
Mehak Khan <mehakikhan2081@gmail.com>
2024-10-07 23:55:42 +0500

    Data added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
+import {useEffect} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {FiSettings} from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
+
 
 function App() {
   return (
